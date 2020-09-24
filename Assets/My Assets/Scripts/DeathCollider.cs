c11e41443a494c88b6e0f5302736d6dab93d8224
ochoajulian <52212040+ochoajulian@users.noreply.github.com>
2020-09-24 14:29:49 -0600

    Added Death-dealing collider for river, not in perfect place yet

---
+++ Assets/My Assets/Scripts/DeathCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoverShooter
+{
+public class DeathCollider : MonoBehaviour
+{
+    public GameObject cowboy;
+    CharacterEffects characterEffects;
+    CharacterHealth characterHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterEffects = cowboy.GetComponent<CharacterEffects>();
+        characterHealth = cowboy.GetComponent<CharacterHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        characterHealth.Deal(200f);
+        characterEffects.OnDead();
+    }
+}
+}
